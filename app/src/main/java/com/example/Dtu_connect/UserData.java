06673ff56c07bc234f218628511edf04d3da115b
Aryan <aryans07024@gmail.com>
2020-09-29 08:40:54 +0530

    Profile page with about

---
+++ app/src/main/java/com/example/Dtu_connect/UserData.java
@@ -0,0 +1,18 @@
+package com.example.Dtu_connect;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class UserData implements Serializable {
+
+    String discription;
+    String branch;
+    String batch;
+    ArrayList<String> language;
+    ArrayList<String> skill;
+    ArrayList<String> projectname;
+    ArrayList<String> projecturl;
+    String email;
+    String name;
+
+}
